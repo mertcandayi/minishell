a79893637fb5348f8ca7fce13ab61a8ddd877a6b
medayi <mrtcn15.dy@gmail.com>
2025-04-26 01:17:57 +0300

    shell ve ötesi

---
+++ includes/minishell.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: medayi    <medayi@student.42istanbul>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/26 01:09:21 by medayi            #+#    #+#             */
+/*   Updated: 2025/04/26 01:09:21 by medayi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <dirent.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <sys/ioctl.h>
+# include <termios.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <termcap.h>
+
+#endif
